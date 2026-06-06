ebf9a4a441d7978c44c6485ecded31a120cb706f
Praveen775D <upraveen776@gmail.com>
2026-06-06 22:36:26 +0530

    feat: add outreach email template

---
+++ src/test_template.py
@@ -0,0 +1,8 @@
+from utils.email_templates import generate_email
+
+print(
+    generate_email(
+        "John",
+        "google.com"
+    )
+)
---
+++ src/utils/email_templates.py
@@ -0,0 +1,18 @@
+def generate_email(
+    name,
+    company
+):
+
+    return f"""
+Hi {name},
+
+I noticed {company} and wanted to connect.
+
+We help companies automate
+their outbound lead generation.
+
+Would you be interested in a short discussion?
+
+Regards,
+Praveen
+"""
